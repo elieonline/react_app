4f9899deead90d822418bf1474a8ee5fa30db170
Elijah Adeniyi <dammyelie@yahoo.com>
2022-01-09 11:48:54 +0100

    react custom hooks

--- src/App.js
+++ src/App.js
@@ -1,55 +1,82 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 const title = "React";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const useSemiPersistState = (key, initialState) => {
+  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
 
 const App = () => {
-  const handleChange = (event) => {
-    console.log(event.target.value);
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useSemiPersistState("search", "React");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1> Hello {title} </h1>
-      <label htmlFor="search"> Search: </label>{" "}
-      <input id="search" type="text" onChange={handleChange} />
+      <Search onSearch={handleSearch} searchTerm={searchTerm} />
       <hr />
-      <List />
+      <List list={searchedStories} />
     </div>
   );
 };
 
-const List = () => {
-  return list.map((item) => {
-    return (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
-    );
-  });
+const Search = ({ onSearch, searchTerm }) => {
+  return (
+    <div>
+      <label htmlFor="search"> Search: </label>{" "}
+      <input id="search" type="text" value={searchTerm} onChange={onSearch} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+    </div>
+  );
 };
+
+const List = ({ list }) =>
+  list.map((item) => <Item key={item.objectID} item={item} />);
+
+const Item = ({ item }) => (
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>
+);
 
 export default App;
